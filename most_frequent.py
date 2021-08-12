fdf1afd3fc9a4876b83a0d033ee77db4e33b810e
amateurcoder666 <71907601+amateurcoder666@users.noreply.github.com>
2021-08-12 19:02:28 +0530

    Update most_frequent.py

--- most_frequent.py
+++ most_frequent.py
@@ -2,28 +2,18 @@ str = input("Please enter a string : ")
 str = str.lower()
 
 def freqsort(freq):
-    sortedfreq = sorted(freq.items(), key = lambda kv: kv[1],reverse=True)
+    sortedfreq = sorted(freq.items(),key = lambda kv: kv[1], reverse=True)
     freqdict = dict(sortedfreq)
     for i,j in freqdict.items():
         print(i,'=',j,end =" ")        
 
-def dictinsert(lst):
-    freq = {}
-    for i in range(0,len(str)):
-        if(lst[ord(str[i])-ord("a")]!= 0):
-            freq[str[i]] = lst[ord(str[i])-ord("a")]
-            lst[ord(str[i]) - ord('a')] = 0           
-    freqsort(freq)
-
 def frequency(str):
-    lst = []
-    for i in range(0,26):
-        lst.append(0)
-    for i in range (0,len(str)): 
-        lst[ord(str[i])-ord("a")] += 1
-    dictinsert(lst) 
-frequency(str)   
-
-
-        
-    
+ freq = {}
+ for i in range(0,len(str)):
+     freq[str[i]] = 0
+ for i in range(0,len(str)):
+     freq[str[i]] = freq.get(str[i]) + 1
+ 
+ freqsort(freq)
+ 
+frequency(str)
